bbc5a5aaade0c913220f9b4c68d927baaab113ad
karthxk07 <karthikkalyanam07@gmail.com>
2024-03-06 23:55:47 +0530

    Added moving cursor

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import { Canvas } from "@react-three/fiber";
 import Background from "./Components/Background";
 import Layout from "./Components/Layout";
 import Sidebar from "./Components/Sidebar";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const cursorFollowerRef = useRef();
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      setTimeout(() => {
+        cursorFollowerRef.current.style.left = `${e.clientX - 10}px`;
+        cursorFollowerRef.current.style.top = `${e.clientY - 10}px`;
+      }, 100);
+    });
+  }, []);
   return (
     <>
       <Sidebar />
       <Layout />
+      <div
+        ref={cursorFollowerRef}
+        className="backdrop-invert fixed transition  duration-200 ease-in rounded-full z-100 aspect-square h-12  "
+      ></div>
     </>
   );
 }
